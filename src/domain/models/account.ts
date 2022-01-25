7f49aaacf0db36fb531d47572df9952cfb722a5d
LennonOliveira29 <lennon.oliveira@useaurea.com>
2022-01-24 21:17:05 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/domain/models/account.ts
+++ src/domain/models/account.ts
@@ -1,5 +1,6 @@
 export interface AccountModel {
   id: string
   name: string
+  email: string
   password: string
 }
---
+++ src/infra/db/mongodb/account-repository/account-repository.ts
@@ -0,0 +1,21 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { AccountModel } from '../../../../domain/models/account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const { insertedId } = await accountCollection.insertOne(accountData)
+
+    const findById = await accountCollection.findOne(insertedId)
+    const { _id, ...accountWithoutId } = findById
+    const { name, email, password, id } = Object.assign({}, accountWithoutId, { id: _id.toHexString() })
+    return {
+      name,
+      email,
+      password,
+      id
+    }
+  }
+}
