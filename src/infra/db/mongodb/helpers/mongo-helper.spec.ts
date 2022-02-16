fa886174f055ae37f148fc4490b6ef48d0faa27f
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-02-16 11:41:55 -0300

    feat: ensure LogControllerDecorator calls controllers with correct value

--- src/infra/db/mongodb/helpers/mongo-helper.spec.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -4,9 +4,11 @@ describe('Mongo helper', () => {
   beforeAll(async () => {
     await sut.connect(process.env.MONGO_URL)
   })
+
   afterAll(async () => {
     await sut.disconnect()
   })
+
   test('Should reconnect if mongodb is down', async () => {
     let accountCollection = await sut.getCollection('accounts')
     expect(accountCollection).toBeTruthy()
