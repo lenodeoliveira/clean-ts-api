6430bcd2afaa7cf6ccc604cfd84dc8171acfa6a6
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-03-02 20:40:09 -0300

    refactor: change the environment variable name from MONGO_URL to MONGO_URI

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,7 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
+
+// mongodb+srv://lennon-oliveira:<foVMTYSSHnW&29>@cluster0.k2yhn.mongodb.net/clean-node-api?retryWrites=true&w=majority
