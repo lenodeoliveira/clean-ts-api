e6b710354ef4aff1a4006b251ff63fbe1088d43d
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-03-02 14:25:21 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -51,5 +51,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'lennon@gmail.com',
+          password: '12345'
+        })
+        .expect(401)
+    })
   })
 })
