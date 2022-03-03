1cb2a1a25d603866022bfdd86193ba1b6b3b9966
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-03-03 14:10:38 -0300

    refactor: rename environment variable

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config()
 export default {
-  mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URI || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
