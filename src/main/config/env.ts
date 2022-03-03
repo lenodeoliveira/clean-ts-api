a0c158d41ae626703a40556e65c749a4d251d6e3
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-03-03 11:15:32 -0300

    refactor: remove url in file env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,3 @@ export default {
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
-
-// mongodb+srv://lennon-oliveira:<foVMTYSSHnW&29>@cluster0.k2yhn.mongodb.net/clean-node-api?retryWrites=true&w=majority
