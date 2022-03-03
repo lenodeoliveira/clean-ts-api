2f99bbed08508ec825b68ad14cd69f84bd73af48
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-03-03 14:09:03 -0300

    chore: add lib dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config()
 export default {
   mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
