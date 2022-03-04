c6080bdb0b3c664099d34c0a2dab6434a4428249
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-03-04 11:48:35 -0300

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config()
 export default {
-  mongoUrl: process.env.MONGO_URI || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URI || 'mongodb://mongo:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
