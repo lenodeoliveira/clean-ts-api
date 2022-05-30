ec90457c1110117a6936da65c77cde71005af445
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-05-30 18:19:42 -0300

    refactor: organize main layer folders

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
-import { adaptRoute } from '../adapters/express/express-route-adapter'
-import { makeSignUpController } from '../factories/controller/signup/signup-controller-factory'
-import { makeLoginController } from '../factories/controller/login/login-controller-factory'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeSignUpController } from '../factories/controller/login/signup/signup-controller-factory'
+import { makeLoginController } from '../factories/controller/login/login/login-controller-factory'
 
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
-import { adaptRoute } from '../adapters/express/express-route-adapter'
-import { makeAddSurveyController } from '../factories/controller/add-survey/add-survey-controller-factory'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeAddSurveyController } from '../factories/controller/survey/add-survey/add-survey-controller-factory'
 
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
