a08b435747d5695d27b827bc3d5789c1c0712c5e
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-06-19 13:01:13 -0300

    refactor: move auth middlewares to separate files

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -4,8 +4,6 @@ import { makeSignUpController } from '../factories/controller/login/signup/signu
 import { makeLoginController } from '../factories/controller/login/login/login-controller-factory'
 
 export default (router: Router): void => {
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/signup', adaptRoute(makeSignUpController()))
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/login', adaptRoute(makeLoginController()))
 }
