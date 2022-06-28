2a4a39d84ad60251e507f3f3ee78958409b9cc05
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-06-28 18:51:08 -0300

    test: ensure SurveyResultRepository insert new surveyResult if its new

---
+++ src/infra/db/mongodb/survey/survey-result/survey-result-mongo-repository.spec.ts
@@ -0,0 +1,90 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { SurveyResultRepository } from './survey-result-mongo-repository'
+import { SurveyModel } from '@/domain/models/survey'
+
+let surveyCollection: Collection
+let surveyResultCollection: Collection
+let accountCollection: Collection
+
+const makeSut = (): SurveyResultRepository => {
+  return new SurveyResultRepository()
+}
+
+const makeSurvey = async (): Promise<SurveyModel> => {
+  const { insertedId } = await surveyCollection.insertOne({
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer'
+      },
+      {
+        answer: 'other_answer'
+      }
+    ],
+    date: new Date()
+  })
+  const findById = await surveyCollection.findOne(insertedId)
+  return MongoHelper.map(findById)
+}
+
+const makeAccount = async (): Promise<string> => {
+  const { insertedId } = await accountCollection.insertOne({
+    name: 'Any_name',
+    email: 'any_mail@gmail.com',
+    password: '123'
+  })
+  return insertedId.toHexString()
+}
+
+describe('Survey Result Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    await surveyResultCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('save()', () => {
+    test('Should add a survey result if its new', async () => {
+      const survey = await makeSurvey()
+      const accountId = await makeAccount()
+      const sut = makeSut()
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toBeTruthy()
+      expect(surveyResult.answer).toBe(survey.answers[0].answer)
+    })
+    test('Should add a survey result if its new', async () => {
+      const survey = await makeSurvey()
+      const accountId = await makeAccount()
+      const sut = makeSut()
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toBeTruthy()
+      expect(surveyResult.answer).toBe(survey.answers[0].answer)
+    })
+
+  })
+})
