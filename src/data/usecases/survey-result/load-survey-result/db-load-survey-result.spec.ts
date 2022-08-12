0152c91fa926dab9895e5a0dc8ef1df127144d0d
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-08-12 15:11:45 -0300

    refactor: move LoadSurveyResult dependencies to a protocols file

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -1,6 +1,6 @@
-import { mockLoadSurveyResultRepository } from '@/data/test'
-import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 import { DbLoadSurveyResult } from './db-load-survey-result'
+import { LoadSurveyResultRepository } from './db-load-survey-result-protocols'
+import { mockLoadSurveyResultRepository } from '@/data/test'
 
 type SutTypes = {
   sut: DbLoadSurveyResult
