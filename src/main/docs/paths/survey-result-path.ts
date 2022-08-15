bd586075addaf7c31f74f3007733176bf29b6150
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-08-15 19:52:10 -0300

    docs: update requirements and add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -5,6 +5,7 @@ export const surveyResultPath = {
     }],
     tags: ['Enquetes'],
     summary: 'API para criar a resposta de uma enquete',
+    description: 'Essa rota só pode ser executada por usuário autenticado',
     parameters: [{
       in: 'path',
       name: 'surveyId',
@@ -22,6 +23,44 @@ export const surveyResultPath = {
         }
       }
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: [] as any
+    }],
+    tags: ['Enquetes'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description: 'Essa rota só pode ser executada por usuário autenticado',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      description: 'ID da enquete a ser respondida',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
     responses: {
       200: {
         description: 'Sucesso',
