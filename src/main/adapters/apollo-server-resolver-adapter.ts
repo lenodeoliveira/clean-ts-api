1ae9e1e6e43633d87f9a62fa526291907ffcce1b
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-09-08 18:11:36 -0300

    feat: add signUp Mutation

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -4,7 +4,7 @@ import { Controller } from '@/presentation/protocols'
 export const adaptResolver = async (controller: Controller, args: any): Promise<any> => {
   const httpResponse = await controller.handle(args)
   switch (httpResponse.statusCode) {
-    case 200: return httpResponse.body
+    case 200:
     case 204: return httpResponse.body
     case 400: throw new UserInputError(httpResponse.body.message)
     case 401: throw new AuthenticationError(httpResponse.body.message)
