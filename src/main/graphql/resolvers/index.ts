5adb5799b27fbcd09eb1b0c4828751f3720d4d62
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-09-09 10:39:55 -0300

    feat: add survey schema

--- src/main/graphql/resolvers/index.ts
+++ src/main/graphql/resolvers/index.ts
@@ -1,5 +1,9 @@
+import base from './base'
 import login from './login'
+import survey from './survey'
 
 export default [
-  login
+  base,
+  login,
+  survey
 ]
--- src/main/graphql/type-defs/index.ts
+++ src/main/graphql/type-defs/index.ts
@@ -1,7 +1,9 @@
 import base from './base'
 import login from './login'
+import survey from './survey'
 
 export default [
   base,
-  login
+  login,
+  survey
 ]
