c197abc34a0449de8b901d30f1170fe7c4f559af
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-09-09 13:42:49 -0300

    feat: add SurveyResult query and Mutation

--- src/main/graphql/resolvers/index.ts
+++ src/main/graphql/resolvers/index.ts
@@ -1,9 +1,11 @@
 import base from './base'
 import login from './login'
 import survey from './survey'
+import surveyResult from './survey-result'
 
 export default [
   base,
   login,
-  survey
+  survey,
+  surveyResult
 ]
